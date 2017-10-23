45575de330fd3b1015f75f83f5a28338bfe91762
onereallylongname <antonioalmeida2099@gmail.com>
2017-10-23 16:14:09 +0100

    edited help. Refactored initialize, global vars, created way to define input path and output path. Next: refactor read_file, add global var with reconized patterns and reconize file types. refine arduino vs .c vs esp8266 vs others

--- code_esp8266_c_cpp_to_html.rb
+++ code_esp8266_c_cpp_to_html.rb
@@ -1,20 +1,39 @@
 # encoding: Iso-8859-1
 
 class FileCToHtml
-  Version = "2.0" # read multiple files
-  Help = "Reads files and looks for my headrs and de \"server.on\" command to write a html file.
-          -v/-version Version
-          -h/-help Help
-          Files will appear in the order of the arguments
-          Note: This code was created to work with arduino esp8266 code!"
+  # Global Vars
+  Version = "2.5"
+  Help = "Reads .c, .cpp. ino, and maybe .js files. Looks for costum commentaries to write a html file.
+          This file makes an index containing all the funtions in the file and links them to it's description.
+          -v/-version   Version
+          -h/-help      Help
+          -i            Set input path
+          -o            Set output path"
+  FILE_TYPES = ['c', 'cpp', 'ino', 'js']
 
   def initialize argsIn
     if argsIn.empty?; puts "No valid args! Try -h"; return; end
-    if argsIn[0].eql? "-v" or argsIn[0].eql? "-version"; puts Version; return; end
-    if argsIn[0].eql? "-h" or argsIn[0].eql? "-help"; puts Help; return; end
+    @classObj = self
+    @options = {:h => false, :v => false, :i => './', :o => './', :f => []}
 
-    @readFileNameArray = []
-    argsIn.each { |names| @readFileNameArray.push(names.to_s) }
+    read_argv_flags argsIn
+
+    if @options[:v]; puts Version; return; end
+    if @options[:h]; puts Help; return; end
+    if @options[:f].empty?; puts 'Exit: file types not suported.'; return; end
+
+
+    p @options
+
+    # case option
+    #
+    # when condition
+    #
+    # end
+puts 'estou aqui'
+#return  #TODO: retirar return
+  #  @readFileNameArray = []
+  #  argsIn.each { |names| @readFileNameArray.push(names.to_s) }
 
     #TODO: make it run once
     @fileCode = ""
@@ -22,32 +41,65 @@ class FileCToHtml
     @fileHeader = "<a href=\"#aurl\"> Avaliable url </a> <br>"
     @webUrl = "<a name=\"aurl\" <h3 style=\"color: #ff9933\"> Avaliable url: </h3> </a> <br> "
     @urlNumber = 0
-    @readFileNameArray.each do |name|
-      if File.file?(name)
-        print "Reading: #{name}"
+
+    @options[:f].each do |name|
+      if File.file?(make_path :i, name)
+        puts"Reading: #{name}"
         readMyFile name
+        puts"Writing: #{name} to html"
         writeToMyFile name
       else
-        puts  "File #{name} does not exist"
+        puts  "File #{make_path :i,name} does not exist"
       end
     end
   end
 
 private
 
+  def make_path inOut, name=''
+    return @options[inOut] + '/' + name
+  end
+
+  def strip_to_sym strIn
+    return strIn[1].to_sym
+  end
+
+  def known_file_type fileName
+    fileKnown = false
+    fileType = fileName.split('.')[-1]
+    FILE_TYPES.each { |ft| fileKnown |= fileType.eql? ft}
+    return fileKnown
+  end
+
+  def read_argv_flags argsIn
+    skipVal = argsIn.length + 1
+    argsIn.each_with_index do |argIn, ind|
+      next if skipVal == ind
+      arg = argIn.downcase()
+      if arg[0].eql? '-'
+        symAgr = strip_to_sym(arg)
+        if @options[symAgr].is_a? String
+          @options[symAgr] = argsIn[ind + 1]
+          skipVal = ind + 1
+        elsif @options[symAgr] == false
+          @options[symAgr] = true
+        end
+      elsif known_file_type arg
+        @options[:f] << argIn.gsub(/(\.\/)|(\.\\)/,'')
+      end
+      puts argIn
+    end
+  end
+
   def getStyle
     return File.read('css/style.css') if File.file?('css/style.css')
     raise "CSS file not found!"
   end
 
-  def split_name fileName
-    newName = 'CodeFile'
-    if fileName.split('\\').size > fileName.split('/').size
-      newName = fileName.split('\\')
-    else
-     newName = fileName.split('/')
-   end
-   return newName
+  def rename_out fileName
+    name = fileName.split('/')[-1]
+    name = name.split('\\')[-1]
+   return "Code_From_#{name.gsub(/\./,'_')}.html"
   end
 
   def disply_percente line, numLines
@@ -59,13 +111,13 @@ private
 
 #TODO: refactor readMyFile
   def readMyFile fileName
-    numLines = File.foreach(fileName).count
+    numLines = File.foreach(make_path( :i, fileName)).count
     lineCounter = 0
     precenteDone = 0
     counter = 0
     headerc = false
     firstFunc = true
-    file = File.new(fileName, "r:Iso-8859-1")
+    file = File.new(make_path( :i, fileName), "r:Iso-8859-1")
     while (line = file.gets)
       lineCounter = disply_percente lineCounter, numLines
       a = line[0..3]
@@ -115,18 +167,19 @@ private
     file.close
     @fileCode = @fileCode + "<br> <font color=\"#ff9933\"> - - - - - \"\" - - - - - </font> <br>"
     @fileHeader = @fileHeader + "<br> <br>"
+    puts 'Done (1/2)'
   end
 
   def writeToMyFile fileName
-    newName = split_name fileName
-    codeFileNewname = "Code_From_#{newName[-1].split('.')[-2]}.html"
+    codeFileNewname = rename_out fileName
+    puts codeFileNewname
     style = getStyle
     #TODO: refactor header (create function)
     htmlHeader = "<!DOCTYPE html>\n <html>\n    <head>\n      <style>\n#{style}\n      </style>\n    </head>"
     htmlBody = "<body> <div id=\"content\"> <div id=\"sidebar\"> <h2 style=\"color:white\"> Contents </h2>" + @fileHeader + "</div> <div id=\"main\"> <div class=\"container\" id=\"title\"> <h1 style=\"color: #ff9933\"> Code functions </h1> <font color=\"#ff9933\"> (Version: " + Time.now.to_s  + ") </font> </div> <div class=\"container\" id=\"main1\"> <br> "  + @webUrl + @fileCode + " </div> </div> </div> <footer> <p>Code By: AA @ <a href=\"https://github.com/onereallylongname\"> github</a></p></footer> </body> </html>"
 #TODO: change output dir
-    File.write(codeFileNewname, htmlHeader + htmlBody)
-    puts " Done!"
+    File.write(make_path(:o) + codeFileNewname, htmlHeader + htmlBody)
+    puts " Done (2/2)!"
   end
 
 end
