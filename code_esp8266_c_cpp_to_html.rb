7a5eae58c30be2421b2254cb8058a2b8c0a59a3f
onereallylongname <antonioalmeida2099@gmail.com>
2017-10-24 15:58:22 +0100

    changed a lot...

--- code_esp8266_c_cpp_to_html.rb
+++ code_esp8266_c_cpp_to_html.rb
@@ -8,19 +8,24 @@ class FileCToHtml
           -v/-version   Version
           -h/-help      Help
           -i            Set input path
-          -o            Set output path"
+          -o            Set output path
+          -e/-esp       Before a file that uses the Arduino esp8266 syntax"
+          # -t            Set page title. Default \"Code functions\"" # not implemented
   FILE_TYPES = ['c', 'cpp', 'ino', 'js']
-  LANGS = {:cLike => {'//>' => :T1, '//>>' => :T2, '//>>>' => :T3, 'args' => :args, '//args' => :args,
-                      'rtrn' => :rtrn, '//rtrn' => :rtrn, 'dscr' => :dscr, '//dscr' => :dscr,
-                      '/****' => :startC, '****' => :endC}}
+  LANGS = {:cLike => {'//1>' => :T1, '//2>' => :T2, '//3>' => :T3, 'args:' => :args, '//args:' => :args, 'rtrn:' => :rtrn, '//rtrn:' => :rtrn, 'dscr:' => :dscr, '//dscr:' => :dscr,
+  '/***' => :startB, '***/' => :endB, '#include' => :incl}}
+  TAGS_TRANSLATOR = {:p => 'p', :T1 => 'h1', :T2 => 'h2', :T3 => 'h2', :args => 'args', :rtrn => 'rtrn', :dscr => 'dscr'}
+  TTLS = [:T1,:T2, :T3] # Title headers
+  DESC = [:dscr, :args, :rtrn] # Code descriptors
+  # CMNT = [:startC, :endC] # Comment identifier # '/*' => :startC, '*/' => :endC,
+  BLCK = [:startB, :endB] # Comment Bloks
 
-  TAGS_TRANSLATOR = {:T1 => 'h1', :T2 => 'h3', :T3 => 'h5', :args => 'args', :rtrn => 'rtrn', :dscr => 'dscr'}
 
   def initialize argsIn
     # Return if no arguments where given
     if argsIn.empty?; puts "No valid args! Try -h"; return; end
     # Create hash with options
-    @options = {:h => false, :v => false, :i => './', :o => './', :f => []}
+    @options = {:h => false, :v => false, :i => './', :o => './', :f => [], :e => []}
     # Process argv into options
     read_argv_flags argsIn
     # Check for version, help or if there are no files to run
@@ -77,6 +82,8 @@ private
           skipVal = ind + 1
         elsif @options[symAgr] == false
           @options[symAgr] = true
+        elsif @options[symAgr].is_a? Array
+          @options[symAgr] = argsIn[ind + 1]
         end
       elsif known_file_type arg
         @options[:f] << argIn.gsub(/(\.\/)|(\.\\)/,'')
@@ -96,11 +103,14 @@ private
    return "Code_From_#{name.gsub(/\./,'_')}.html"
   end
   # Displays progress in read stage.
-  def disply_percente line, numLines
-    precenteDone = (line*100/numLines).to_i
-    print (" " + precenteDone.to_s + "%") if (precenteDone % 25 == 0 and precenteDone > 0)
+  def disply_percente line, numLines, lastNum
+    precenteDone = (line*100/numLines)
+    print '0%' if line == 0
+    print (" " + precenteDone.to_s + "%") if (precenteDone.ceil % 25 == 0 and precenteDone > 0) and lastNum.to_s != precenteDone.to_s
     print " ." if line%10 == 0
-    return (line += 1)
+    puts '100%' if line == numLines-1
+    newLastNum = precenteDone
+    return (line += 1), newLastNum
   end
 
   def find_lang name
@@ -116,69 +126,112 @@ private
     end
   end
 
-  def text_tag marker, lang
-    return 'p' if TAGS_TRANSLATOR[LANGS[lang][marker.strip]].nil?
-    return TAGS_TRANSLATOR[LANGS[lang][marker.strip]]
+  def addIncluds
+    return "<h2 class=\"maincolor\"> Includes </h2>" + @includes + '<br>' if @includes != ""
+    return ""
   end
 
-  def find_line_content line
-    return line[4..-1].gsub(/[^0-9A-Za-z\-_]/, '')
-  end
-
-  def addToSideBar lineContente, textTag
-    @sileSideBar += "<#{textTag}> <a href=\"##{lineContente.downcase}\"> #{lineContente}  </a> </#{textTag}> <br>"
-  end
-
-  def addToBody lineContente, textTag, header=false
-    if header
-      @sileSideBar = "<#{textTag}> <a href=\"##{nameTag.downcase}\"> #{nameTag}</a><#{textTag}>"
-    else
-#TODO: copy add to body
+  def text_tags line, lang
+    textTags = nil
+    keyLessLine = line
+    LANGS[lang].keys.each do |marker|
+      if keyLessLine.include? marker
+        textTags = LANGS[lang][marker]
+        keyLessLine.gsub!(marker, " ")
+      end
+      break if !textTags.nil?
     end
+    keyLessLine.strip!
+    textTags = ':p' if !keyLessLine.empty? and textTags.nil?
+  return textTags, keyLessLine
   end
 
-#TODO: refactor readMyFile
+  def make_link lineContente, hashTagsFound
+    link = (lineContente.gsub(/[^0-9A-Za-z\-_\/]/, '').downcase) + '0'
+    while  hashTagsFound.include? link
+      link[-1] = (link[-1].to_i + 1).to_s
+    end
+    return link
+  end
+
+  def addToSideBar lineContente, textTag, hashTagsFound
+    link = make_link lineContente, hashTagsFound
+    @sileSideBar = @sileSideBar + "<#{TAGS_TRANSLATOR[textTag]}> <a href=\"##{link}\"> #{lineContente}  </a> </#{TAGS_TRANSLATOR[textTag]}>"
+    return link
+  end
+
+  def addToBody lineContente, textTag, link=nil, inBlock=0, inComment=false
+    if TTLS.include? textTag
+      @fileBody += "<#{TAGS_TRANSLATOR[textTag]} class=\"maincolor\"> <a name=\"#{link}\"> #{lineContente} </a></#{TAGS_TRANSLATOR[textTag]}><br>"
+    elsif textTag == :incl
+        @includes += "<font class=\"comment\">#{lineContente.gsub(/[^0-9A-Za-z\-_\/]/, '')}</font><br>"
+    elsif inBlock
+      if DESC.include? textTag
+        @fileBody += "<font class=\"maincolor\">#{TAGS_TRANSLATOR[textTag]}</font>: #{lineContente}<br>"
+      elsif BLCK.include? textTag
+        inComment = !inComment
+      elsif !BLCK.include? textTag
+        if inComment
+          @fileBody += "<font class=\"comment\">#{lineContente}</font><br>"
+        else
+          @fileBody += "#{lineContente}<br>"
+        end
+      end
+    end
+    return inComment
+  end
+
+  def find_block textTag, inBlock
+    inBlock = true if BLCK[0] == textTag
+    inBlock = false if BLCK[1] == textTag
+    return inBlock
+  end
+
   def readMyFile fileName, lang
     numLines = File.foreach(make_path( :i, fileName)).count
     hashTagsFound = []
-    @fileBody = ""
-    @sileSideBar = "<a href=\"#aurl\"> Avaliable url </a> <br>"
-    @webUrl = "<a name=\"aurl\" <h3 class=\"maincolor\"> Avaliable url: </h3> </a> <br> "
-    @urlNumber = 0
     lineCounter = 0
     precenteDone = 0
     counter = 0
-    headerc = false
-    firstFunc = true
-
-    localOptions = {}
-
+    inBlock = 0
+    inComment = false
+    lastNum = 0;
+    @sileSideBar = ""
+    @webUrl = ""
+    @urlNumber = 0
+    @fileBody = ""
+    @includes = ""
+    esp = false
+    @fileBody = ""
+    if @options[:e].include? fileName
+      @sileSideBar = "<a href=\"#aurl\"> Avaliable url </a> <br>"
+      @webUrl = "<a name=\"aurl\" <h3 class=\"maincolor\"> Avaliable url: </h3> </a> <br> "
+      @urlNumber = 0
+      esp = true
+      puts 'Mode: esp on.'
+    end
     file = File.new(make_path( :i, fileName), "r:Iso-8859-1")
-
-
     while (line = file.gets)
+      lineCounter, lastNum = disply_percente lineCounter, numLines, lastNum
+      textTags, lineContente = text_tags line, lang
+      inBlock = find_block textTags, inBlock
+ # p lineCounter
+ # p textTags
+# p keyLessLine
+ #system('pause')
+# next
 
-      lineCounter = disply_percente lineCounter, numLines
-
-
-      a = line[0..4]
-
-      textTag = text_tag line[0..4], lang
-
-p textTag if textTag != 'p'
-
-      if [:T1, :T2, :T3].include? textTag
-        counter = 2
-        lineContente = find_line_content line
-        addToSideBar lineContente, textTag
-        addToBody lineContente, textTag
-
-
-
-
-
-
-
+      if TTLS.include? textTags
+          link = addToSideBar lineContente, textTags, hashTagsFound
+          hashTagsFound << link
+      end
+      inComment = addToBody lineContente, textTags, link, inBlock, inComment
+    end
+    file.close
+    @sileSideBar = @sileSideBar + "<br> <br>"
+    puts 'Done (1/2)'
+  end
+=begin
       if a.eql? "//>>"
         counter = 2
         headerc = true
@@ -221,21 +274,16 @@ p textTag if textTag != 'p'
         @webUrl = @webUrl + "<br>" if @urlNumber % 10 == 0
       end
     end
-    file.close
-    @fileBody = @fileBody + "<br> <font class=\"maincolor\"> - - - - - \"\" - - - - - </font> <br>"
-    @sileSideBar = @sileSideBar + "<br> <br>"
-    puts 'Done (1/2)'
-  end
+=end
 
   def writeToMyFile fileName
     codeFileNewname = rename_out fileName
-    puts codeFileNewname
     style = getStyle
     #TODO: refactor header (create function)
-    htmlHeader = "<!DOCTYPE html>\n <html>\n    <head>\n      <style>\n#{style}\n      </style>\n    </head>"
-    htmlBody = "<body> <div id=\"content\"> <div id=\"sidebar\"> <h2 class=\"sidecolor\"> Contents </h2>" + @sileSideBar + "</div> <div id=\"main\"> <div class=\"container\" id=\"title\"> <h1 class=\"maincolor\"> Code functions </h1> <font class=\"maincolor\"> (Version: " + Time.now.to_s  + ") </font> </div> <div class=\"container\" id=\"main1\"> <br> "  + @webUrl + @fileBody + " </div> </div> </div> <footer> <p>Code By: AA @ <a href=\"https://github.com/onereallylongname\"> github</a></p></footer> </body> </html>"
+    htmlHeader = "<!DOCTYPE html> <html> <head> <title> #{codeFileNewname.split('.')[0]} </title> <style> #{style} </style> </head>"
+    htmlBody = "<body> <div id=\"content\"> <div id=\"sidebar\"> <div id=\"innersidebar\"> <h2> Contents </h2> <br>" + @sileSideBar + "</div></div> <div id=\"main\"> <div id=\"maininnerdiv\"> <div class=\"container\" id=\"title\"> <h1 class=\"maincolor\"> #{codeFileNewname.split('.')[0]} </h1> <font class=\"maincolor\"> (Version: " + Time.now.to_s  + ") </font> </div> <div class=\"container\" id=\"main1\"> <br> " + @webUrl + addIncluds + @fileBody + " </div> </div></div> </div> <footer> <p>Code By: AA @ <a href=\"https://github.com/onereallylongname\"> github</a></p></footer> </body> </html>"
     File.write(make_path(:o) + codeFileNewname, htmlHeader + htmlBody)
-    puts " Done (2/2)!"
+    puts "Done (2/2): #{codeFileNewname}"
   end
 
 end
